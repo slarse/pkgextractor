c409b04a7e4b0b802322cb842ae19e95eeafc6df
slarse <slarse@kth.se>
2020-05-15 14:11:20 +0200

    Use regex to extract package from source file

--- src/main/java/se/slarse/pkgextractor/Main.java
+++ src/main/java/se/slarse/pkgextractor/Main.java
@@ -1,16 +1,15 @@
 package se.slarse.pkgextractor;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.objectweb.asm.ClassReader;
-import spoon.Launcher;
-import spoon.reflect.CtModel;
-import spoon.reflect.declaration.CtPackageDeclaration;
-import spoon.support.compiler.FileSystemFile;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -40,17 +39,15 @@ public class Main {
         return String.join(".", Arrays.copyOf(classname, classname.length - 1));
     }
 
-    private static String parseSourceFilePackage(Path sourceFile) {
-        Launcher launcher = new Launcher();
-        launcher.getEnvironment().setLevel("OFF");
-        launcher.getEnvironment().setNoClasspath(true);
-        launcher.addInputResource(new FileSystemFile(sourceFile.toFile()));
-        CtModel model = launcher.buildModel();
-
-        CtPackageDeclaration pkgDecl = model.getRootPackage().getFactory()
-                .CompilationUnit().getMap()
-                .values().iterator().next()
-                .getPackageDeclaration();
-        return pkgDecl.getReference().getQualifiedName();
+    private static String parseSourceFilePackage(Path sourceFile) throws IOException {
+        List<String> lines = Files.readAllLines(sourceFile);
+        Pattern pattern = Pattern.compile("\\s*package\\s+?(\\S+)\\s*;");
+        for (String line : lines) {
+            Matcher matcher = pattern.matcher(line);
+            if (matcher.matches()) {
+                return matcher.group(1);
+            }
+        }
+        return "";
     }
 }
